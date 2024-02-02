8be96dd8c2c0ea86cbfbfce591bb87c8b4caab5e
Millzy <millzyyt08@gmail.com>
2024-02-02 22:01:27 +0000

    VC++ styling con D:

--- MelonSeed/src/main.cpp
+++ MelonSeed/src/main.cpp
@@ -2,10 +2,10 @@
 
 #define SEED_API extern "C" __declspec(dllexport)
 
-SEED_API void init() {
+SEED_API void Init() {
     MessageBoxA(nullptr, "Inited", "Inited", MB_OK);
 }
 
-SEED_API void late_init() {
+SEED_API void LateInit() {
     MessageBoxA(nullptr, "Late Inited", "Late Inited", MB_OK);
 }
