1e54d5dd24c948ce131e29b184f9cd06079c364a
Millzy <millzyyt08@gmail.com>
2024-02-02 20:42:11 +0000

    plugin that does nothingggg

---
+++ MelonSeed.Plugin/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+
+namespace MelonSeed.Plugin
+{
+    public class Plugin : MelonPlugin
+    {
+        public override void OnApplicationEarlyStart()
+        {
+            // load dll thingy
+        }
+
+        public override void OnPreModsLoaded()
+        {
+            // thingyyyy
+        }
+    }
+}
