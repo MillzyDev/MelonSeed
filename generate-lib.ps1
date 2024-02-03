6519055235ebbc821ed5966d9d5cf8679b394f51
Millzy <millzyyt08@gmail.com>
2024-02-03 16:50:49 +0000

    rename file

--- generate-lib.ps1
+++ generate-lib.ps1
@@ -1,20 +1,20 @@
 param(
     [Parameter(Mandatory=$true)]
-    [string] $dllInFilepath
+    [string] $dllInFilepath,
 
-    #[Parameter(Mandatory=$true)]
-    #[string] $libOutFilepath
+    [Parameter(Mandatory=$true)]
+    [string] $libOutFilepath
 )
 
 $moduleName = [io.path]::GetFileNameWithoutExtension($dllInFilepath) 
 $exportsFilename = $moduleName + ".exports"
 
+# get a list of exported functions
 & dumpbin.exe /nologo /exports "$dllInFilepath" > $exportsFilename
 
 $exportContent = Get-Content -Path ./$exportsFilename
 
 #create a list of exported functions
-
 $exports = New-Object System.Collections.Generic.List[string]
 foreach ($line in $($exportContent -split "`r`n")) {
     if ($line -match "^\s*\d+\s+[0-9a-fA-F]+\s+(?:[0-9a-fA-F]+\s+)?(.+?)(?=\s+\(forwarded|\s*$)") {
@@ -24,6 +24,7 @@ foreach ($line in $($exportContent -split "`r`n")) {
     }
 }
 
+# format into a .def
 $moduleDefFilename = $moduleName + ".def"
 $moduleDef = "EXPORTS`n"
 
@@ -33,6 +34,5 @@ foreach ($export in $exports) {
 
 Out-File -FilePath "./$moduleDefFilename" -InputObject $moduleDef
 
-$libFilename = $moduleName + ".lib"
-
-& lib.exe /nologo /def:$moduleDefFilename /MACHINE:x64 /out:$libFilename
+# create .lib from .def
+& lib.exe /nologo /def:$moduleDefFilename /MACHINE:x64 /out:$libOutFilepath
