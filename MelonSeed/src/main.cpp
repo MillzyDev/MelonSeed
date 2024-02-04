39d7ee6d899991d731cdf577e066b76b8c7b357d
Millzy <millzyyt08@gmail.com>
2024-02-04 19:27:30 +0000

    remove unnecessary function

--- MelonSeed/src/main.cpp
+++ MelonSeed/src/main.cpp
@@ -1,13 +1,7 @@
-#include <windows.h>
-
-#include <iostream>
+#include "modloader.hpp"
 
 #define SEED_API extern "C" __declspec(dllexport)
 
 SEED_API void Init() {
-    std::cout << "Hi from C++!";
-}
-
-SEED_API void LateInit() {
-    std::cout << "Hello again from C++!";
+    modloader_init();
 }
