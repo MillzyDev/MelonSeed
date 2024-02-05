ed7899d5f32f051ee25e4e94324eb65566c2ee30
Millzy <millzyyt08@gmail.com>
2024-02-05 22:29:02 +0000

    plugin base

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,14 @@
+using MelonLoader;
+
+namespace MelonSeed
+{
+    // ReSharper disable once UnusedType.Global
+    public class Plugin : MelonPlugin
+    {
+        public override void OnPreInitialization()
+        {
+            // TODO: Load LLVM std lib stuff
+            // TODO: Initialise MelonSeed
+        }
+    }
+}
