6ece145867cc72d4f7d86f33e0018448771631b9
Millzy <millzyyt08@gmail.com>
2024-02-05 22:34:05 +0000

    fix versions in plugin

--- Plugin/Properties/AssemblyInfo.cs
+++ Plugin/Properties/AssemblyInfo.cs
@@ -4,17 +4,17 @@ using System.Runtime.InteropServices;
 using MelonLoader;
 using MelonSeed;
 
-[assembly: AssemblyTitle("MelonSeed.Plugin")]
+[assembly: AssemblyTitle("MelonSeed")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany(null!)]
-[assembly: AssemblyProduct("MelonSeed.Plugin")]
+[assembly: AssemblyProduct("MelonSeed")]
 [assembly: AssemblyCopyright("Copyright © Frederick Mills  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: MelonInfo(typeof(Plugin), "MelonSeed", "0.1.0", "Millzy")]
