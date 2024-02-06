b02fb3f4565c63a7a7a492411f4d2627bf1084bd
Millzy <millzyyt08@gmail.com>
2024-02-06 23:49:35 +0000

    todo list

--- MelonSeed/src/main.cpp
+++ MelonSeed/src/main.cpp
@@ -1,9 +1,19 @@
 #define MELON_SEED_ENTRYPOINT extern "C" __declspec(dllexport)
 
 MELON_SEED_ENTRYPOINT void melon_seed_init() {
+    // initialise logger
 
+    // install other hooks (Scene loaded or smth, just some callbacks more or less)
+
+    // open libraries
+
+    // open mods
+    // construct mods
+    // load mods
 }
 
 MELON_SEED_ENTRYPOINT void melon_seed_start() {
-
+    // start mods
+    // ???
+    // profit
 }
